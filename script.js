097231a7d54ff4cc712bda8d97fbc85c9f367512
Mathews P Mathew <mathewsparan@gmail.com>
2022-05-03 17:48:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function redirect(str) {
+    switch (str) {
+        case 'musk':
+            window.location.href = "https://twitter.com/mathews_pmathew";
+            break;
+        case 'gh':
+            window.location.href = "https://github.com/mathewspmathew";
+            break;
+        case 'link':
+            window.location.href = "https://www.linkedin.com/in/mathews-p-mathew-a091281b7";
+            break;
+        case 'drbl':
+            window.location.href = "https://www.behance.net/gallery/142297339/Competition-poster/modules/803894201";
+            break;
+        default:
+            break;
+    }
+}
